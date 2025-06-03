d1f0e1772efc7d7aefa05a6635208e1cce30fbfc
RositaE6mi <odilemvondo13@gmail.com>
2025-06-03 23:16:04 +0100

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 export const routes: Routes = [
-    
+    {path:"", component:HomeComponent},
+    {path:"", component: NavbarComponent}
 ];
